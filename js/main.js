1a5a02e0daded4b42fbcbfc4e823c09a38b57e4a
eugene-h-kim <h_kim123224@fanshaweonline.ca>
2022-03-02 14:32:38 -0500

    adding lightbox movie

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
 (() => {
-  
+  // collect the elements we need to interact with
+  const sigils = document.querySelectorAll('.sigilContainer'),
+        lightBox = document.querySelector('.lightbox'),
+        houseVideo = lightBox.querySelector('video'),
+        lbClose = lightBox.querySelector('.close'),
+        theBanner = document.querySelector('#houseImages');
+
+  // functions go here
+  function popLightBox () {
+    // debugger;
+    lightBox.classList.add('show-lightbox');
+    houseVideo.play();
+  }
+
+  function closeLightBox () {
+    lightBox.classList.remove('show-lightbox');
+    houseVideo.pause();
+    houseVideo.currentTime = 0;
+  }
+
+  function animateBanners () {
+    // figure out how much to move the banner images
+    let bannerWidth = 600,
+        offset = this.dataset.offset;
+
+    let finalAmount = bannerWidth * offset;
+
+    theBanner.style.right = finalAmount + 'px';
+
+    console.log(finalAmount);
+  }
+
+  // add event handling
+  // sigils.forEach(sigil => sigil.addEventListener('click', popLightBox));
+  sigils.forEach(sigil => sigil.addEventListener('click', animateBanners));
+
+  // close the lightbox when the video reaches its end
+  houseVideo.addEventListener('ended', closeLightBox);
+  lbClose.addEventListener('click', closeLightBox);
 })();
